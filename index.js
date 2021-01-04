0d086422b2a786b8a28ce0c2bc827a4fb3bf533d
Gustavo Laranjeira <68796123+gustavolaranjeira@users.noreply.github.com>
2021-01-04 14:19:39 -0300

    First commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const PORT = process.env.PORT || 3000;
 const express = require("express");
+
 const apiRoute = require("./routes/api");
 
 const path = require("path");
@@ -9,6 +9,4 @@ const app = express();
 app.use("/api", apiRoute);
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-    console.log("Servidor rodando na porta: ", PORT);
-});
+app.listen(process.env.PORT || 8080 || 3000);
